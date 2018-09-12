49a4c99eabd7de0a556f1dd946f35d9a231182a7
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-12 13:58:39 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 def first_challenge
   contacts = {
-    "Jon Snow" => {
+    key1: "Jon Snow" => {
       name: "Jon",
       email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
-    "Freddy Mercury" => {
+    key2: "Freddy Mercury" => {
       name: "Freddy",
       email: "freddy@mercury.com",
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
